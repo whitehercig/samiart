e695deb481d687c04b5e5012b251cca043ecd0af
whitehercig <157502719+whitehercig@users.noreply.github.com>
2025-05-04 18:43:16 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-    // Функция для открытия модального окна
     function openModal(imageSrc) {
       var modal = document.getElementById("myModal");
       var modalImg = document.getElementById("img01");
@@ -6,13 +5,11 @@
       modalImg.src = imageSrc;
   }
 
-  // Функция для увеличения/уменьшения изображения
   function toggleZoom(modal) {
       var img = modal.querySelector(".modal-content");
       img.classList.toggle("zoomed");
   }
 
-  // Закрытие модального окна при клике вне изображения
   document.getElementById("myModal").addEventListener("click", function(event) {
       if (event.target === this) {
           this.style.display = "none";
